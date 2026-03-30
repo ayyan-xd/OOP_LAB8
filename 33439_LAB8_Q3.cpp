97e99667b760096389dd4ea82d762f9ef91fcd6a
Muhammad Ayyan Ibrahim <ayyanibrahim44@gmail.com>
2026-03-30 15:45:27 +0500

    Update 33439_LAB8_Q3.cpp

--- 33439_LAB8_Q3.cpp
+++ 33439_LAB8_Q3.cpp
@@ -4,17 +4,18 @@
 using namespace std;
 
 class DivisionByZeroException : public exception {
-public:
+public :
     const char* what() const noexcept override {
-        return "Error: Cannot divide by zero!";
+        return "ERROR : Division by Zero not allowed.";
     }
 };
+
 double performDivision(double numerator, double denominator) {
     if (denominator == 0) {
         throw DivisionByZeroException();
     }
     if (denominator < 0) {
-        cout << "[Warning]: Converting negative denominator to positive..." << endl;
+        cout << "WARNING : Converting negative denominator to positive." << endl;
         denominator = -denominator;
     }
     return numerator / denominator;
@@ -24,27 +25,26 @@ int main() {
     char choice;
     do {
         double num, den;
-        cout << "Division Operation : " << endl;
         cout << "Enter numerator: ";
         while (!(cin >> num)) {
-            cout << "Invalid input. Please enter a number: ";
+            cout << "Invalid input. Please enter a number : ";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
         cout << "Enter denominator: ";
         while (!(cin >> den)) {
-            cout << "Invalid input. Please enter a number: ";
+            cout << "Invalid input. Please enter a number : ";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
         try {
             double result = performDivision(num, den);
-            cout << "Result: " << result << endl;
+            cout << "Result : " << result << endl;
         } 
-        catch (const DivisionByZeroException& e) {
+        catch(const DivisionByZeroException &e) {
             cout << e.what() << endl;
         }
-        cout << "\nDo you want to perform another division? (y/n): ";
+        cout << "\nDo you want to perform another division? (y/n) : ";
         cin >> choice;
     } 
     while (choice == 'y' || choice == 'Y');
